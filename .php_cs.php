a71c7dd7eeee4d127d32ed67310a577e537341e6
Martin Henriksen <martin.henriksen.92@gmail.com>
2019-07-06 01:05:28 +0200

    add php cs fixer to circleci

---
+++ .php_cs.php
@@ -0,0 +1,34 @@
+<?php
+
+$excludeDirs = [
+    'bootstrap/',
+    'config/',
+    'public/',
+    'resources/',
+    'storage/',
+    'vendor/',
+];
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude($excludeDirs)
+    ->ignoreDotFiles(true)->ignoreVCS(true)
+;
+
+return PhpCsFixer\Config::create()
+    ->setRiskyAllowed(true)
+    ->setRules(
+        [
+            '@PhpCsFixer' => true,
+            '@PhpCsFixer:risky' => true,
+            '@PSR1' => true,
+            '@PSR2' => true,
+            'align_multiline_comment' => true,
+            'blank_line_before_return' => true,
+        ]
+    )
+    ->setCacheFile(__DIR__.'/.php_cs.cache')
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in(__DIR__)
+    );
